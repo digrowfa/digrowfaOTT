2a8d1d782fe1984f42e2538485a11020533d85f8
Himanshu <sharmagi1998@gmail.com>
2022-12-11 16:34:00 +0530

    theme chager

---
+++ src/Pages/Common/Includes/Navigation.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const Navigation = () => {
+    return (
+        <>
+            <Nav>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-xl-2 col-lg-2 col-md-2 col-sm-12 col-12'>
+
+                        </div>
+                        <div className='col-xl-10 col-lg-10 col-md-10 col-sm-12 col-12'></div>
+                    </div>
+                </div>
+            </Nav>
+        </>
+    )
+}
+
+export default Navigation
--- src/Routes/Routers.js
+++ src/Routes/Routers.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from '../Pages/Common/Home'
 import Login from '../Pages/Common/LoginRegister/Login'
 import Register from '../Pages/Common/LoginRegister/Register'
+import Switch from "react-switch";
 
 
 export const ThemeContext = createContext(null)
@@ -39,9 +40,21 @@ const Routers = () => {
 
                         {/* =========end========== */}
                     </Routes>
-                    {/* <div className='themeSwitch'>
-                        <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
-                    </div> */}
+                    <div className='themeSwitch'>
+                        <label htmlFor="material-switch">
+                            <Switch
+                                checked={theme === "dark"}
+                                onChange={toggleTheme}
+                                handleDiameter={45}
+                                uncheckedIcon={false}
+                                checkedIcon={false}
+                                height={40}
+                                width={80}
+                                className="react-switch"
+                                id="material-switch"
+                            />
+                        </label>
+                    </div>
                 </div>
 
             </ThemeContext.Provider>
